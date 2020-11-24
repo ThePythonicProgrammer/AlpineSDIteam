e020569f00924d02a4c100a5cd499068befadf2e
Liam <liam@debian.acme>
2020-11-24 13:31:51 -0700

    removing scrollspy for the moment

--- main.js
+++ main.js
@@ -4,15 +4,7 @@ document.addEventListener('DOMContentLoaded', function() {
   var carousels = M.Carousel.init(elems, options);
 });
 
-// Scrollspy Implementation
-document.addEventListener('DOMContentLoaded', function() {
-    var elms = document.querySelectorAll('.scrollspy');
-    var scrollspy = M.ScrollSpy.init(elems, options);
-  });
-
 function carousel_move(){
   carousels.next(1)
 }
-var active = scrollspy.getActiveElement()
-console.log(active)
 setInterval(carousel_move, 5000)

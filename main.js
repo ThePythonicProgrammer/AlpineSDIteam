e548042d4144016d38a00dd38c3fb31d15f6e6db
Liam <liam@debian.acme>
2020-11-24 11:36:15 -0700

    trying it with jquery, old system works but new one doesn't

--- main.js
+++ main.js
@@ -1,18 +1,15 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.carousel');
-    var instances = M.Carousel.init(elems, options);
-  });
+$(document).ready(function(){
+  $('.carousel').carousel();
+});
 
-// var main-carousel = M.Carousel.getInstance('.main-carousel');
-//
-// var time = 1;
-//
-// var main-carousel = setInterval(function() {
-//    if (time <= 3) {
-//       main-carousel.next(1);
-//       time++;
-//    }
-//    else {
-//       clearInterval(interval);
-//    }
-// }, 5000);
+var time = 1;
+
+var interval = setInterval(function() {
+   if (time <= 3) {
+      $('.carousel').carousel('next', 1);
+      time++;
+   }
+   else {
+      clearInterval(interval);
+   }
+}, 5000);

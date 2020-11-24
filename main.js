95ea22cbfecf097a4a4986df3723bee34d27e6b6
Liam <liam@debian.acme>
2020-11-24 11:30:31 -0700

    Second try at carousel system

--- main.js
+++ main.js
@@ -3,16 +3,16 @@ document.addEventListener('DOMContentLoaded', function() {
     var instances = M.Carousel.init(elems, options);
   });
 
-var main-carousel = M.Carousel.getInstance('.main-carousel');
-
-var time = 1;
-
-var main-carousel = setInterval(function() {
-   if (time <= 3) {
-      main-carousel.next(1);
-      time++;
-   }
-   else {
-      clearInterval(interval);
-   }
-}, 5000);
+// var main-carousel = M.Carousel.getInstance('.main-carousel');
+//
+// var time = 1;
+//
+// var main-carousel = setInterval(function() {
+//    if (time <= 3) {
+//       main-carousel.next(1);
+//       time++;
+//    }
+//    else {
+//       clearInterval(interval);
+//    }
+// }, 5000);

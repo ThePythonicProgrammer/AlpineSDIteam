e0537c2a2b4f9166d53b14b2f0a0ed5a8384c562
Liam <liam@debian.acme>
2020-11-24 11:24:52 -0700

    trying out new carousel system

--- main.js
+++ main.js
@@ -1,7 +1,18 @@
-$(document).ready(function(){
-  $('.carousel').carousel();
-});
-$('.carousel.carousel-slider').carousel({
-    fullWidth: true,
-    indicators: true
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.carousel');
+    var instances = M.Carousel.init(elems, options);
   });
+
+var main-carousel = M.Carousel.getInstance('.main-carousel');
+
+var time = 1;
+
+var main-carousel = setInterval(function() {
+   if (time <= 3) {
+      main-carousel.next(1);
+      time++;
+   }
+   else {
+      clearInterval(interval);
+   }
+}, 5000);

652132f739e77ee395c096a34bc787d483a7b758
Liam <liam@debian.acme>
2020-11-24 12:02:09 -0700

    implementing scrollspy to prevent carousel motion if not on that section

--- main.js
+++ main.js
@@ -2,8 +2,16 @@ $(document).ready(function(){
   $('.carousel').carousel();
 });
 
+$(document).ready(function(){
+    $('.scrollspy').scrollSpy();
+  });
+
 function carousel_move(){
   $('.carousel').carousel('next', 1);
+}
+
+function carousel_recognition(id) {
+  return 'a[href="#' + id + '"]'
 }
 
 setInterval(carousel_move, 5000)

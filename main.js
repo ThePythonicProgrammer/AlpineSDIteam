e620164a20a341d9713296eab24ff8fc692fb7e6
Liam <liam@debian.acme>
2020-11-24 11:38:57 -0700

    jquery may not be working, checking with a simpler method.

--- main.js
+++ main.js
@@ -2,14 +2,14 @@ $(document).ready(function(){
   $('.carousel').carousel();
 });
 
-var time = 1;
-
-var interval = setInterval(function() {
-   if (time <= 3) {
-      $('.carousel').carousel('next', 1);
-      time++;
-   }
-   else {
-      clearInterval(interval);
-   }
-}, 5000);
+// var time = 1;
+//
+// var interval = setInterval(function() {
+//    if (time <= 3) {
+//       $('.carousel').carousel('next', 1);
+//       time++;
+//    }
+//    else {
+//       clearInterval(interval);
+//    }
+// }, 5000);

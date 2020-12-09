db9c9928a3f37b33b478c492337f56b3b856c451
Liam <liam@debian.acme>
2020-12-09 13:32:56 -0700

    changing name and adding js file for rocket

---
+++ branches/rocket.js
@@ -0,0 +1,9 @@
+// Adding parallax
+$(document).ready(function(){
+    $('.parallax').parallax();
+  });
+
+// Dropdown Implementation
+$(document).ready(function(){
+  $('.dropdown-trigger').dropdown();
+})

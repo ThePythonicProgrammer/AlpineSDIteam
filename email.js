8be59f62059914f3cc19100cac9e128af5c2639b
Gaston <liam@giffordclan.org>
2021-05-20 20:21:53 -0600

    Temp removing email.js, adding new carousel features (initCarousel)

--- email.js
+++ email.js
@@ -1,36 +1,36 @@
-document.addEventListener("DOMContentLoaded", function() {
-  fields.first_name = document.getElementById('first_name');
-  fields.last_name = document.getElementById('last_name');
-  fields.email = document.getElementById('email');
-  fields.response = document.getElementById('response');
-})
-
-class User {
-  constructor(first_name, last_name, email, response) {
-    this.first_name = first_name;
-    this.last_name = last_name;
-    this.email = email;
-    this.response = response;
-  }
-}
-
-function sendContact() {
-  let usr = new User(first_name.value, last_name.value, email.value, response.value);
-
-  alert(`${usr.first_name} thanks for registering.`)
-}
-
-window.onload = function sendEmail() {
-            document.getElementById('contact-form').addEventListener('submit', function(event) {
-                event.preventDefault();
-                // generate a five digit number for the contact_number variable
-                this.contact_number.value = Math.random() * 100000 | 0;
-                // these IDs from the previous steps
-                emailjs.sendForm('contact_service', 'contact_form', this)
-                    .then(function() {
-                        console.log('SUCCESS!');
-                    }, function(error) {
-                        console.log('FAILED...', error);
-                    });
-            });
-        }
+// document.addEventListener("DOMContentLoaded", function() {
+//   fields.first_name = document.getElementById('first_name');
+//   fields.last_name = document.getElementById('last_name');
+//   fields.email = document.getElementById('email');
+//   fields.response = document.getElementById('response');
+// })
+//
+// class User {
+//   constructor(first_name, last_name, email, response) {
+//     this.first_name = first_name;
+//     this.last_name = last_name;
+//     this.email = email;
+//     this.response = response;
+//   }
+// }
+//
+// function sendContact() {
+//   let usr = new User(first_name.value, last_name.value, email.value, response.value);
+//
+//   alert(`${usr.first_name} thanks for registering.`)
+// }
+//
+// window.onload = function sendEmail() {
+//             document.getElementById('contact-form').addEventListener('submit', function(event) {
+//                 event.preventDefault();
+//                 // generate a five digit number for the contact_number variable
+//                 this.contact_number.value = Math.random() * 100000 | 0;
+//                 // these IDs from the previous steps
+//                 emailjs.sendForm('contact_service', 'contact_form', this)
+//                     .then(function() {
+//                         console.log('SUCCESS!');
+//                     }, function(error) {
+//                         console.log('FAILED...', error);
+//                     });
+//             });
+//         }
--- main.js
+++ main.js
@@ -14,6 +14,14 @@ $(document).ready(function(){
   $('.dropdown-trigger').dropdown();
 })
 
+// Carousel Images
+function initCarousel(){
+	var carousel = $('#main-carousel')
+	for (var i=0; i<6; i++;){ // change the less than to increase the number of items in the carousel
+		carousel.innerHTML += '<a class="carousel-item" href="#'+i+'!"><img src="assets/carousel/'i'.jpg" \n' //make sure all images are jpg's or else this will break
+	}
+}
+initCarousel()
 // Carousel Motion
 function carousel_move(){
   $('.carousel').carousel('next', 1);

b91e0bf589cc5db0a1fb6cd7ff509b4c682903e6
Liam <liam@giffordclan.org>
2021-05-20 21:44:57 -0600

    replacing the method

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
 // Carousel Implementation
 function initCarousel(){
 	var carousel = $('#main-carousel')
-	//for (var i=0; i<6; i++) { // change the less than to increase the number of items in the carousel
-	carousel.innerHTML = '<a class="carousel-item" href="#1!"><img src="assets/carousel/1.jpg" /></a>\n' //make sure all images are jpg's or else this will break
-	//}
+	for (var i=0; i<6; i++) { // change the less than to increase the number of items in the carousel
+	//carousel.innerHTML = '<a class="carousel-item" href="#1!"><img src="assets/carousel/1.jpg" /></a>\n' //make sure all images are jpg's or else this will break
+  var item = document.createElement("a");
+  item.id = i
+  item.classList += "carousel-item"
+  carousel.appendChild(item);
+
+  var img = document.createElement("img");
+  img.src = "assets/carousel/"+i+".jpg"
+  document.getElementById(i).appendChild(img);
+  }
 }
 initCarousel()
 

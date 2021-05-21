65d5e3f759db7daae547e57a00ff87f8ebe97419
Liam <liam@giffordclan.org>
2021-05-20 22:21:10 -0600

    moved into directory structure, fixed issues with collapsible...

--- js/main.js
+++ js/main.js
@@ -4,11 +4,14 @@ function initCarousel(){
 
   var fixedItem = document.createElement("div");
   fixedItem.classList.add("carousel-fixed-item", "center")
+  fixedItem.id = "fixedItem"
   carousel.appendChild(fixedItem);
 
-  var fixedButton = document.createElement("button");
+  var fixedButton = document.createElement("a");
   fixedButton.classList.add("btn", "waves-effect", "white", "grey-text", "darken-text-2")
-  document.getElementsByClassName('carousel-fixed-item').appendChild(fixedButton)
+  fixedButton.innerHTML = "Join the Discord!"
+  fixedButton.href = "http://www.discord.gg/PGMUsbh"
+  document.getElementById('fixedItem').appendChild(fixedButton)
 
   for (var i=1; i<6; i++) { // change the less than to increase the number of items in the carousel
 	//carousel.innerHTML = '<a class="carousel-item" href="#1!"><img src="assets/carousel/1.jpg" /></a>\n' //make sure all images are jpg's or else this will break
@@ -25,7 +28,9 @@ function initCarousel(){
 initCarousel()
 
 $(document).ready(function(){
-  $('.carousel').carousel();
+  $('.carousel').carousel({
+    indicators: true,
+  });
 });
 
 // Standard Implementation
@@ -39,11 +44,18 @@ $(document).ready(function(){
   $('.dropdown-trigger').dropdown();
 })
 
-// Carousel Images
-
+// Collapsible Implementation
+$(document).ready(function(){
+    $('.collapsible').collapsible();
+  });
 
 // Carousel Motion
 function carousel_move(){
  $('.carousel').carousel('next', 1);
 };
 setInterval(carousel_move, 5000);
+
+var scrollButton = document.getElementById("scrollTop");
+scrollButton.addEventListener("click", function(){
+  window.scrollTo(0,0);
+})
